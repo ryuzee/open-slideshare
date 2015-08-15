8f98b4b6b2bc3594237ba21ddc025444e2622a2e
Ryutaro YOSHIBA <ryuzee@gmail.com>
2015-08-15 12:16:43 +0900

    refs #26

---
+++ app/Test/Case/Model/UserTest.php
@@ -0,0 +1,95 @@
+<?php
+
+App::uses('User', 'Model');
+
+/**
+ * User Test Case.
+ */
+class UserTest extends CakeTestCase
+{
+    /**
+     * Fixtures.
+     *
+     * @var array
+     */
+    public $fixtures = array(
+        'app.user',
+    );
+
+    /**
+     * base_data
+     *
+     * @var array
+     */
+    public $base_data = array(
+        'username' => 'hoge@example.com',
+        'password' => '12345678',
+        'display_name' => 'test',
+        'biography' => 'test',
+    );
+
+    /**
+     * setUp method.
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $this->User = ClassRegistry::init('User');
+    }
+
+    /**
+     * validationProvider
+     *
+     */
+    public function validationProvider()
+    {
+        return array(
+            array($this->base_data, true),
+            array(array_merge($this->base_data, array('username' => 'hoge@example.com')), true),
+            array(array_merge($this->base_data, array('username' => '')), false),
+            array(array_merge($this->base_data, array('username' => 'hoge')),  false),
+            array(array_merge($this->base_data, array('username' => 'hoge2')), false),
+            array(array_merge($this->base_data, array('username' => 'test@example.com')), false), // already used
+            array(array_merge($this->base_data, array('password' => '12345')), true),
+            array(array_merge($this->base_data, array('password' => '!#$%&*+@')), true),
+            array(array_merge($this->base_data, array('password' => '[]`"(){}<>/.,_')), false),
+            array(array_merge($this->base_data, array('password' => '1234')), false),
+            array(array_merge($this->base_data, array('password' => '')), false),
+            array(array_merge($this->base_data, array('display_name' => '')), false),
+            array(array_merge($this->base_data, array('biography' => '')), false),
+        );
+    }
+
+    /**
+     * @dataProvider validationProvider
+     */
+    public function testValidation($a, $expected)
+    {
+        $this->User->set($a);
+        $result = $this->User->validates();
+        $this->assertEqual($result, $expected);
+    }
+
+    /**
+     * testPasswordMustBeHashed
+     *
+     */
+    public function testPasswordMustBeHashed()
+    {
+        $this->User->set($this->base_data);
+        $this->User->save();
+        $id = $this->User->getLastInsertID();
+        $data = $this->User->findById($id);
+        $this->assertNotEquals($this->base_data['password'], $data['User']['password']);
+    }
+
+    /**
+     * tearDown method.
+     */
+    public function tearDown()
+    {
+        unset($this->User);
+
+        parent::tearDown();
+    }
+}
