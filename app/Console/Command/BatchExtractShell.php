f64958f7f774bc57b18eb02b051e12f4be2a2f05
Ryutaro YOSHIBA <ryuzee@gmail.com>
2015-08-19 15:30:14 +0900

    fix coding style [ci skip]

--- app/Console/Command/BatchExtractShell.php
+++ app/Console/Command/BatchExtractShell.php
@@ -3,16 +3,51 @@
 App::uses('ComponentCollection', 'Controller');
 App::uses('S3Component', 'Controller/Component');
 
+/**
+ * Class: BatchExtractShell.
+ */
 class BatchExtractShell extends AppShell
 {
+    /**
+     * uses.
+     *
+     * @var array
+     */
     public $uses = array('Slide');
 
+    /**
+     * components.
+     *
+     * @var mixed
+     */
+    public $components;
+
+    /**
+     * S3.
+     *
+     * @var mixed
+     */
+    public $S3;
+
+    /**
+     * Slide.
+     *
+     * @var mixed
+     */
+    public $Slide;
+
+    /**
+     * startup.
+     */
     public function startup()
     {
         $this->components = new ComponentCollection();
         $this->S3 = $this->components->load('S3');
     }
 
+    /**
+     * main.
+     */
     public function main()
     {
         $slides = $this->Slide->find('all');
--- app/Console/Command/ExtractShell.php
+++ app/Console/Command/ExtractShell.php
@@ -3,25 +3,61 @@
 App::uses('ComponentCollection', 'Controller');
 App::uses('S3Component', 'Controller/Component');
 
+/**
+ * Class: ExtractShell
+ *
+ */
 class ExtractShell extends AppShell
 {
+    /**
+     * tasks
+     *
+     * @var array
+     */
     public $tasks = array('SQS.QueueWorker');
 
+    /**
+     * components.
+     *
+     * @var mixed
+     */
+    public $components;
+
+    /**
+     * S3.
+     *
+     * @var mixed
+     */
+    public $S3;
+
+    /**
+     * startup
+     *
+     */
     public function startup()
     {
         $this->components = new ComponentCollection();
         $this->S3 = $this->components->load('S3');
     }
 
+    /**
+     * main
+     *
+     */
     public function main()
     {
         $this->QueueWorker->addFunction('extract', $this, 'handleJob');
         $this->QueueWorker->work();
     }
 
+    /**
+     * handleJob
+     *
+     * @param mixed $data
+     */
     public function handleJob($data)
     {
-        echo '[LOG] Start extracting '.$data['key']."\n";
+        echo '[LOG] Start extracting ' . $data['key'] . "\n";
 
         return $this->S3->extract_images($data);
     }
