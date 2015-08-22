155a283f2dfa3cfe81621d56b8bb03122e23b82b
Ryutaro YOSHIBA <ryuzee@gmail.com>
2015-08-23 08:13:37 +0900

    fix tests for the environment which does not have openoffice installed

--- app/Test/Case/Controller/Component/SlideProcessingComponentTest.php
+++ app/Test/Case/Controller/Component/SlideProcessingComponentTest.php
@@ -44,14 +44,27 @@ class SlideProcessingComponentTest extends CakeTestCase
 
         // pptx
         $result = $method->invoke($this->SlideProcessing, $dir . DS . "test.pptx");
-        $this->assertTrue(in_array($result, array('application/vnd.openxmlformats-officedocument.presentationml.presentation', 'application/vnd.ms-powerpoint')));
+        $this->assertTrue(in_array($result,
+            array(
+                'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+                'application/vnd.ms-powerpoint',
+                'application/vnd.ms-office',
+            )
+        ));
+
+        // ppt
+        $result = $method->invoke($this->SlideProcessing, $dir . DS . "test.ppt");
+        $this->assertTrue(in_array($result,
+            array(
+                'application/vnd.ms-powerpoint',
+                'application/vnd.ms-office',
+            )
+        ));
 
         // pdf
         $result = $method->invoke($this->SlideProcessing, $dir . DS . "test.pdf");
         $this->assertEqual('application/pdf', $result);
 
-        // ppt
-        $result = $method->invoke($this->SlideProcessing, $dir . DS . "test.ppt");
-        $this->assertEqual('application/vnd.ms-powerpoint', $result);
+
     }
 }
