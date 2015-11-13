fe462b17de766dd55c2952ce0c3a574b14fc3f2f
Ryutaro YOSHIBA <ryuzee@gmail.com>
2015-11-13 19:08:18 +0900

    add phpdoc

--- app/Lib/Event/SQSListener.php
+++ app/Lib/Event/SQSListener.php
@@ -2,14 +2,28 @@
 
 App::uses('CakeEventListener', 'Event');
 
+/**
+ * Class: SQSListener
+ *
+ * @see CakeEventListener
+ */
 class SQSListener implements CakeEventListener {
 
+    /**
+     * implementedEvents
+     *
+     */
     public function implementedEvents() {
         return array(
             'Queue.beforeWork' => 'before_procedure',
         );
     }
 
+    /**
+     * before_procedure
+     *
+     * @param mixed $event
+     */
     public function before_procedure($event) {
         CakeLog::write('debug', 'waiting for 60 seconds...');
         sleep(60);
